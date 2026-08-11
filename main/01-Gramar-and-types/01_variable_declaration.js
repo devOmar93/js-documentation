4cd6caa090320ff132084ef6de746ec123841ddf
Ledy Omar Condori Mestas <ledy.condori@entelgy.com>
2026-08-11 18:09:44 -0500

    feat: add 01_variable_declaration

---
+++ main/01-Gramar-and-types/01_variable_declaration.js
@@ -0,0 +1,52 @@
+/**** DECLARACIÓN DE VARIABLES E INICIALIZACIÓN ****/
+
+/** En una instrucción como let x = 42, la parte "let x" se llama declaración
+    y la otra parte "= 42" se llama inicializador . La declaración permite acceder
+    a la variable más adelante en el código sin lanzar una excepción ReferenceError,
+    mientras que el inicializador asigna un valor a la variable. En las declaraciones
+    var y let, el inicializador es opcional. Si una variable se declara sin un inicializador,
+    se le asigna el valor undefined. **/
+
+/** JavaScript tiene tres tipos de declaraciones de variables. **/
+
+/* Declara una variable, inicializándola opcionalmente con un valor. */
+var
+
+/* Declara una variable local con ámbito de bloque, inicializándola opcionalmente con un valor. */
+let
+
+/* Declara una constante con nombre, de ámbito de bloque y de solo lectura.
+   La inicialización es obligatario */
+const
+
+// Ejemplos de declaraciones de variables sin inicializarlas
+var name;
+let surname;
+
+console.log('name:', name);
+console.log('surname:', surname);
+
+// Ejemplos de declaraciones de variables e inicializadas
+var age = 20;
+let nationality = 'Peruano';
+const address = 'Calle Entelgy piso 14';
+
+console.log('age:', age);
+console.log('nationality:', nationality);
+console.log('address:', address);
+
+
+// Bonus: Puedes declarar variables para desempaquetar valores
+// usando la sintaxis de desestructuración.
+
+// Ejemplo:
+
+// Creamos un objeto llamado "persona" para realizar el ejemplo
+const persona = {
+    name: 'Omar',
+    surname: 'Condori',
+    phoneNumber: '987897899'
+}
+
+const { phoneNumber } = persona;
+console.log('phoneNumber:', phoneNumber);
